5093716e1ef447bf26dc0fa27b73496c0bdcf3c8
OnceaAndreea <andreea.oncea@microfocus.com>
2023-01-19 17:48:08 +0200

    got test based on a string containing className and testName

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import {Octane} from '@microfocus/alm-octane-js-rest-sdk';
 import fs from 'fs';
 import PropertiesReader from 'properties-reader'
 import shell from "shelljs";
-// import simpleGit from "simple-git";
+import Query from "@microfocus/alm-octane-js-rest-sdk/dist/lib/query.js";
 
 const properties = PropertiesReader("./octane-details.properties");
 
@@ -14,17 +14,23 @@ const octane = new Octane({
     password: properties.get("password")
 })
 
-async function getTestById(testId) {
+async function getTestByNameAndClassName(testDetailsAsString) {
     try {
-        const test = await octane.get(Octane.entityTypes.tests).at(testId).fields('name', 'description', 'class_name').execute();
+        // const query = Query.field('class_name')
+        // const test2 = await octane.get(Octane.entityTypes.tests).query(query.build()).execute();
+
+        const testDetailsAsArray = testDetailsAsString.split('#')
+        const className = testDetailsAsArray[0]
+        const testName = testDetailsAsArray[1]
+        const test = await octane.executeCustomRequest(`/api/shared_spaces/1001/workspaces/34003/tests?query="class_name EQ ^${className}^;name EQ ^${testName}^"&fields=name,description,class_name`, Octane.operationTypes.get)
         return test;
     } catch (e) {
         console.log('caught error', e)
     }
 }
 
-async function createCommand(testId) {
-    const test = await getTestById(testId);
+async function createCommand(testDetails) {
+    const test = await getTestByNameAndClassName(testDetails);
     const urlRepo = 'https://github.com/OnceaAndreea/SilkCentralDemo.git'; // will be taken from test
     const folderName = urlRepo.substring(urlRepo.lastIndexOf("/") + 1, urlRepo.indexOf(".git"))
     try {
@@ -37,7 +43,7 @@ async function createCommand(testId) {
 
     const projectPath = '/target'; //will be taken from test
     const classpath = './' + folderName + projectPath + '/*';
-    const command = 'java -cp "' + classpath + '" JUnitCmdLineWrapper ' + test.class_name + ' C:\\Users\\AOncea\\Desktop\\output.xml ' + test.name;
+    const command = 'java -cp "' + classpath + '" JUnitCmdLineWrapper ' + test.data[0].class_name + ' C:\\Users\\AOncea\\Desktop\\output.xml ' + test.data[0].name;
     return command;
 }
 
