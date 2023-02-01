80b6641d620b3f92c1776321ebe25f5abbe22079
OnceaAndreea <andreea.oncea@microfocus.com>
2023-02-01 15:42:43 +0200

    test for class

--- app.js
+++ app.js
@@ -27,19 +27,48 @@ async function getTestByNameAndClassName(testClassName, testMethodName) {
 async function getCommand(testClassName, testMethodName) {
     const test = await getTestByNameAndClassName(testClassName, testMethodName);
     const urlRepo = 'https://github.com/OnceaAndreea/SilkCentralDemo.git'; // will be taken from test
+    const branchName = 'master' //taken from repo
     const folderName = urlRepo.substring(urlRepo.lastIndexOf("/") + 1, urlRepo.indexOf(".git"))
+
+    createClasspathFolder(urlRepo, branchName, folderName)
+    const projectPath = '/target'; //will be taken from test
+    const classpath = './' + folderName + projectPath + '/*';
+    const command = 'java -cp "' + classpath + '" JUnitCmdLineWrapper ' + 'CalculatorTest'
+    return command;
+}
+
+function createClasspathFolder(urlRepo, branchName, folderName) {
     try {
         if (!fs.existsSync("./" + folderName)) {
             shell.exec('git clone ' + urlRepo)
+            if (branchName != null) {
+                shell.cd(folderName)
+                shell.exec('git checkout ' + branchName)
+                shell.exec('git pull ' + urlRepo + " " + branchName)
+                shell.cd('../')
+            }
+        } else {
+            if (branchName === null) {
+                shell.cd(folderName)
+                let branches = shell.exec('git branch').toString()
+                if (branches.indexOf('master') > -1) {
+                    shell.exec('git checkout master')
+                    shell.exec('git pull master')
+                } else {
+                    shell.exec('git checkout main')
+                    shell.exec('git pull main')
+                }
+                shell.cd('../')
+            } else {
+                shell.cd(folderName)
+                shell.exec('git checkout ' + branchName)
+                shell.exec('git pull ' + urlRepo + " " + branchName)
+                shell.cd('../')
+            }
         }
     } catch (e) {
         console.log("An error occurred.")
     }
-
-    const projectPath = '/target'; //will be taken from test
-    const classpath = './' + folderName + projectPath + '/*';
-    const command = 'java -cp "' + classpath + '" JUnitCmdLineWrapper ' + test.data[0].class_name + ' ' + test.data[0].name;
-    return command;
 }
 
 function writeToFile(fileName, command) {
@@ -50,30 +79,42 @@ function writeToFile(fileName, command) {
     })
 }
 
-function getExecutableFile(testsToRun) {
-    const classesAndTestsToRun = testsToRun.split(',');
-    const classAndTestsMap = new Map();
-    classesAndTestsToRun.forEach(function (value) {
-        const classAndTestsToRun = value.split('#')
-        classAndTestsMap.set(classAndTestsToRun[0], classAndTestsToRun[1].split('+'))
+// async function getExecutableFile(testsToRun) {
+//     const classesAndTestsToRun = testsToRun.split(',');
+//     const classAndTestsMap = new Map();
+//     classesAndTestsToRun.forEach(function (value) {
+//         const classAndTestsToRun = value.split('#')
+//         classAndTestsMap.set(classAndTestsToRun[0], classAndTestsToRun[1].split('+'))
+//
+//     });
+//     if (fs.existsSync('./command_to_execute.bat')) {
+//         fs.unlinkSync('./command_to_execute.bat')
+//     }
+//     classAndTestsMap.forEach((value, key) => {
+//         value.forEach(function (val) {
+//             getCommand(key, val).then((command) => {
+//                 if (process.platform === "win32") {
+//                     writeToFile('./command_to_execute.bat', command + "\n")
+//                 } else {
+//                     writeToFile('./command_to_execute.sh', command + "\n")
+//                 }
+//             })
+//         });
+//     })
+//
+// }
 
-    });
-    if (fs.existsSync('./command_to_execute.bat')) {
-        fs.unlinkSync('./command_to_execute.bat')
-    }
-    classAndTestsMap.forEach((value, key) => {
-        value.forEach(function (val) {
-            getCommand(key, val).then((command) => {
+async function getExecutableFile(testsToRun) {
+
+            getCommand('CalculatorTest', 'testAdd').then((command) => {
                 if (process.platform === "win32") {
                     writeToFile('./command_to_execute.bat', command + "\n")
                 } else {
-                    writeToFile('./command-to-execute.sh', command + "\n")
+                    writeToFile('./command_to_execute.sh', command + "\n")
                 }
             })
-        });
-    })
 
-}
+    }
 
 getExecutableFile(process.env.testsToRunConverted)
 
