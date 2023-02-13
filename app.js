7f61375f5f8172891796f44b0d93dd894dc732c6
OnceaAndreea <andreea.oncea@microfocus.com>
2023-02-13 18:11:59 +0200

    target path

--- app.js
+++ app.js
@@ -16,12 +16,16 @@ const octane = new Octane({
 async function getOctaneTest(testPath, testMethod) {
     try {
         let query;
-        if (testPath.lastIndexOf('.') > -1) {
-            const testPackage = testPath.substring(0, testPath.lastIndexOf('.'))
-            const testClass = testPath.substring(testPath.lastIndexOf('.') + 1, testPath.length)
-            query = Query.field('class_name').equal(testClass).and(Query.field('name').equal(testMethod)).and(Query.field('package').equal(testPackage))
+        if (testPath !== "") {
+            if (testPath.lastIndexOf('.') > -1) {
+                const testPackage = testPath.substring(0, testPath.lastIndexOf('.'))
+                const testClass = testPath.substring(testPath.lastIndexOf('.') + 1, testPath.length)
+                query = Query.field('name').equal(testMethod).and(Query.field('class_name').equal(testClass)).and(Query.field('package').equal(testPackage)).and(Query.field('component').equal(Query.NULL))
+            } else {
+                query = Query.field('name').equal(testMethod).and(Query.field('class_name').equal(testPath)).and(Query.field('package').equal(Query.Query.NULL)).and(Query.field('component').equal(Query.NULL))
+            }
         } else {
-            query = Query.field('class_name').equal(testPath).and(Query.field('name').equal(testMethod))
+            query = Query.field('name').equal(testMethod).and(Query.field('class_name').equal(Query.NULL)).and(Query.field('package').equal(Query.NULL)).and(Query.field('component').equal(Query.NULL))
         }
         return await octane.get(Octane.entityTypes.tests).fields('name', 'owner', 'class_name', 'description', 'package', 'classpath_udf', 'branch_udf', 'configuration_type_udf', 'repourl_udf', 'projectpath_udf').query(query.build()).execute();
     } catch (e) {
@@ -34,7 +38,7 @@ async function getListValueById(valueId) {
     return await octane.get(Octane.entityTypes.listNodes).fields('name').query(query.build()).execute();
 }
 
-async function getCommand(testPath, testMethod) {
+async function getCommand(testPath, testMethod, runnerJarPath) {
     const test = await getOctaneTest(testPath, testMethod);
     const configurationTypeValue = await getListValueById(test.data[0].configuration_type_udf.id)
     const configurationTypeName = configurationTypeValue.data[0].name
@@ -45,15 +49,25 @@ async function getCommand(testPath, testMethod) {
     createClasspathFolder(urlRepo, branchName, folderName)
     const projectPath = test.data[0].projectpath_udf;
     const classpath = test.data[0].classpath_udf
-    const totalClasspath = './' + folderName + projectPath + '/*';
+    const rootClasspath = './' + folderName + projectPath;
     const lastIndexOfUnderline = testMethod.lastIndexOf("_")
+
+    const jars = classpath.split(';')
+    let testsClasspath = "";
+    jars.forEach(function (value) {
+        testsClasspath += rootClasspath + value.substring(1, value.length) + ";"
+        console.log(value)
+
+    });
+    testsClasspath = testsClasspath.substring(0, testsClasspath.lastIndexOf(';'))
+
     let command;
     if (configurationTypeName === "isMethod") {
-        command = 'java -cp "' + totalClasspath + '" JUnitCmdLineWrapper ' + testPath + ' ' + testMethod.substring(0, lastIndexOfUnderline)
+        command = 'java -cp "' + testsClasspath + ";" + runnerJarPath + '" JUnitCmdLineWrapper ' + testPath + ' ' + testMethod.substring(0, lastIndexOfUnderline) + ' ' + testMethod
     } else if (configurationTypeName === "isClass") {
-        command = 'java -cp "' + totalClasspath + '" JUnitCmdLineWrapper ' + testPath + ' ' + null;
+        command = 'java -cp "' + testsClasspath + ";" + runnerJarPath + '" JUnitCmdLineWrapper ' + testPath + ' ' + null + ' ' + testMethod
     } else {
-        command = 'java -cp "' + totalClasspath + '" JUnitCmdLineWrapper ' + "RunMeAsAJar" + ' ' + null
+        command = 'java -cp "' + testsClasspath + ";" + runnerJarPath + '" JUnitCmdLineWrapper ' + "RunMeAsAJar" + ' ' + null + ' ' + testMethod
     }
     return command;
 }
@@ -92,7 +106,7 @@ function createClasspathFolder(urlRepo, branchName, folderName) {
     }
 }
 
-async function getExecutableFile(testsToRun) {
+async function getExecutableFile(testsToRun, runnerJarPath) {
     const classesAndTestsToRun = testsToRun.split(',');
     const classAndTestsMap = new Map();
     classesAndTestsToRun.forEach(function (value) {
@@ -108,35 +122,20 @@ async function getExecutableFile(testsToRun) {
         fs.rmdirSync('./testResults', {recursive: true})
     }
 
-    const commandsArray = [];
-
     for (const [testPath, testMethods] of classAndTestsMap) {
         for (const testMethod of testMethods) {
-            await getCommand(testPath, testMethod).then((command) => {
+            await getCommand(testPath, testMethod, runnerJarPath).then((command) => {
                 fs.appendFileSync('./command_to_execute.bat', command + "\n")
             })
         }
     }
-    // let isFirstTestFromASuite = true;
-    // for (const command of commandsArray) {
-    //     const cmd = command.split('JUnitCmdLineWrapper')
-    //     const cmdUpdated = cmd[0] + 'JUnitCmdLineWrapper' + ' ' + isFirstTestFromASuite + cmd[1];
-    //     if (isFirstTestFromASuite === true) {
-    //         isFirstTestFromASuite = false;
-    //     }
-    //     if (process.platform === "win32") {
-    //         fs.appendFileSync('./command_to_execute.bat', cmdUpdated + "\n")
-    //     } else {
-    //         fs.appendFileSync('./command_to_execute.sh', cmdUpdated + "\n")
-    //     }
-    // }
 }
 
-getExecutableFile(process.env.testsToRunConverted)
-// getExecutableFile('domains.animals.AnimalTest#checkCatName_23455+SilkCentralName_1234,domains.jobs.TeacherTest#checkAge_23455')
-
-
-
+getExecutableFile(process.env.testsToRunConverted, process.env.testJarPath)
+// getExecutableFile('#SCNameForJarBased_1234,domains.animals.AnimalTest#checkCatName_23455+SilkCentralName_1234,domains.jobs.TeacherTest#checkAge_23455', 'D:\\Projects\\SilkCentralDemo\\AutomationDemo\\target\\JunitWrapper-1.0-SNAPSHOT-jar-with-dependencies.jar')
+//
+//
+//
 
 
 
